ccf8f85932aee8351acbda62e1a0e485234ab669
Oana-Poran <oanaporan@gmail.com>
2018-12-13 22:07:55 +0200

    button on nav bar added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import MapContainer from './MapContainer';
 import locations from './locations';
+import { FaSearch } from 'react-icons/fa';
+
 import './App.css';
+
 
 class App extends Component {
   state = {
@@ -12,6 +15,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1> Grab a bite near Central Park, NY</h1>
+          <button><FaSearch/></button>
         </header>
         <MapContainer locations={this.state.allLocations}/>
 
