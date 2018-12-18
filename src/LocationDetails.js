f6b83683ddade4203ed3908cb6959e99da74008d
Oana-Poran <oanaporan@gmail.com>
2018-12-18 11:08:59 +0200

    Location details added.

--- src/LocationDetails.js
+++ src/LocationDetails.js
@@ -5,16 +5,21 @@ class LocationDetails extends Component {
         return(
             <div className="location-details">
             <h4>{this.props.locationData.name}</h4>
-            <p>{this.props.locationData.cuisines}</p>
+            <p>Cuisine: {this.props.locationData.cuisines}</p>
 
             {this.props.locationData.average_cost_for_two ? (
                 <p> Average Cost for two : {this.props.locationData.average_cost_for_two}{this.props.locationData.currency}</p>
             ): ""}
+
+            {this.props.locationData.menu_url ? (
+                <a href={this.props.locationData.menu_url}> Checkout the Menu </a>
+            ): ""}
             
             {this.props.locationData.featured_image ? (
-                <div className="logo">
+                <div className="featured-image">
                  <img alt={this.props.locationData.name + 'food picture'}
                       src={this.props.locationData.featured_image} width="100%" height="150" />
+                <p font="small"> Image credit: Zomato</p>
                      </div>
                     ):  ""}
            
