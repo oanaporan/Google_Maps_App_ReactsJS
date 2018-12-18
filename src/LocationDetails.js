ad1de702f2633cee7999786865147142b8d57e2a
Oana-Poran <oanaporan@gmail.com>
2018-12-18 10:14:23 +0200

    LocationDetails added to rendering restaurant info.

--- src/LocationDetails.js
+++ src/LocationDetails.js
@@ -4,10 +4,19 @@ class LocationDetails extends Component {
     render() {
         return(
             <div className="location-details">
-            <h4>{this.props.selectedLocation.name}</h4>
-            <div className="logo">
-            <img alt="restaurant-featured-img" src={this.props.locationData.featured_image} width="100%" height="150" />
-            </div>
+            <h4>{this.props.locationData.name}</h4>
+            <p>{this.props.locationData.cuisines}</p>
+
+            {this.props.locationData.average_cost_for_two ? (
+                <p> Average Cost for two : {this.props.locationData.average_cost_for_two}{this.props.locationData.currency}</p>
+            ): ""}
+            
+            {this.props.locationData.featured_image ? (
+                <div className="logo">
+                 <img alt={this.props.locationData.name + 'food picture'}
+                      src={this.props.locationData.featured_image} width="100%" height="150" />
+                     </div>
+                    ):  ""}
            
         </div>
         )
